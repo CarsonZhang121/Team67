4da1c667ad7323f6edc5bac14d742043174a4075
jiangjun0728 <44887877+jiangjun0728@users.noreply.github.com>
2019-04-03 00:03:36 -0700

    Add files via upload

--- Code/src/main/java/Model/MowerMap.java
+++ Code/src/main/java/Model/MowerMap.java
@@ -4,32 +4,54 @@ import Viewer.Direction;
 import Viewer.Location;
 import Viewer.SquareState;
 
-public class MowerMap {
-    private int width = 201;
-    private int length = 201;
+public class MowerMap extends Lawn{
+    private int width = 601;
+    private int length = 601;
+
+    public void initialMap(int width, int length){
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < length; j++)
+                squares[i][j] = "unkown";
+        }
+    }
 
     //TODO: set square state for the one in mowerMap
     public void setSquare(Location loc, SquareState sqState, Direction d){
-
+        square[loc[0]][loc[1]] = "grass";
     }
 
     //TODO: determine if all the grasses has been cut
     // What is the logic here???
     public Boolean isCompleted(){
-        return false;
+        int grassremain = remainGrassNumber();
+        Boolean isCompleted = false;
+        if(grassremain == 0){
+            isCompleted = true
+        }
+        return isCompleted;
     }
 
     //TODO: get the square state at a specific location
     public SquareState getSquare(Location loc){
-        return SquareState.grass;
+
+        return square[loc[0]][loc[1]];
     }
 
     //TODO: update the map for each scan
     public void updateMapFromScan(String[] neighbors){
 
+        square[currentLoc[0]][currentLoc[1] + 1] = neighbors[0];
+        square[currentLoc[0] + 1][currentLoc[1] + 1] = neighbors[1];
+        square[currentLoc[0] + 1][currentLoc[1]] = neighbors[2];
+        square[currentLoc[0] + 1][currentLoc[1] - 1] = neighbors[3];
+        square[currentLoc[0]][currentLoc[1] - 1] = neighbors[4];
+        square[currentLoc[0] - 1][currentLoc[1] - 1] = neighbors[5];
+        square[currentLoc[0] - 1][currentLoc[1]] = neighbors[6];
+        square[currentLoc[0] - 1][currentLoc[1] + 1] = neighbors[7];
     }
 
     //TODO: extend the map when the mower goes over the boundary
+    //set square = 601 x 601, for maximum size of 200 x 200, no need to expand
     private void expand(){
 
     }
