ab6a0c2b965d3b8092ce9210560f48ed243b0c72
jiangjun0728 <44887877+jiangjun0728@users.noreply.github.com>
2019-04-07 10:44:08 -0700

    4/7/19

--- Code/src/main/java/Model/MowerMap.java
+++ Code/src/main/java/Model/MowerMap.java
@@ -5,20 +5,24 @@ import Viewer.Location;
 import Viewer.SquareState;
 
 public class MowerMap extends Lawn{
-    private int width = 601;
-    private int length = 601;
+    private int width = 60;
+    private int length = 40;
+    SquareState[][] map;
+
 
     public void initialMap(int width, int length){
+
         for(int i = 0; i < width; i++){
             for(int j = 0; j < length; j++){
-                squares[i][j] = "unkown";
+                map[i][j] = SquareState.unknown;
             }
         }
     }
 
     //TODO: set square state for the one in mowerMap
     public void setSquare(Location loc, SquareState sqState, Direction d){
-        square[loc[0]][loc[1]] = "empty";
+
+        map[loc.getX()][loc.getY()] = SquareState.empty;
     }
 
     //TODO: determine if all the grasses has been cut
@@ -27,7 +31,7 @@ public class MowerMap extends Lawn{
         int grassremain = map.remainGrassNumber();
         Boolean isCompleted = false;
         if(grassremain == 0){
-            isCompleted = true
+            isCompleted = true;
         }
         return isCompleted;
     }
@@ -35,24 +39,26 @@ public class MowerMap extends Lawn{
     //TODO: get the square state at a specific location
     public SquareState getSquare(Location loc){
 
-        return square[loc[0]][loc[1]];
+        SquareState localstate;
+        localstate = map[loc.getX()][loc.getY()];
+        return localstate;
     }
 
     //TODO: update the map for each scan
-    public void updateMapFromScan(String[] neighbors){
+    public void updateMapFromScan(Location loc, SquareState[] neighbors){
 
-        square[currentLoc[0]][currentLoc[1] + 1] = neighbors[0];
-        square[currentLoc[0] + 1][currentLoc[1] + 1] = neighbors[1];
-        square[currentLoc[0] + 1][currentLoc[1]] = neighbors[2];
-        square[currentLoc[0] + 1][currentLoc[1] - 1] = neighbors[3];
-        square[currentLoc[0]][currentLoc[1] - 1] = neighbors[4];
-        square[currentLoc[0] - 1][currentLoc[1] - 1] = neighbors[5];
-        square[currentLoc[0] - 1][currentLoc[1]] = neighbors[6];
-        square[currentLoc[0] - 1][currentLoc[1] + 1] = neighbors[7];
+        map[loc.getX()][loc.getY() + 1] = neighbors[0];
+        map[loc.getX() + 1][loc.getY() + 1] = neighbors[1];
+        map[loc.getX() + 1][loc.getY()] = neighbors[2];
+        map[loc.getX() + 1][loc.getY() - 1] = neighbors[3];
+        map[loc.getX()][loc.getY() - 1] = neighbors[4];
+        map[loc.getX() - 1][loc.getY() - 1] = neighbors[5];
+        map[loc.getX() - 1][loc.getY()] = neighbors[6];
+        map[loc.getX() - 1][loc.getY() + 1] = neighbors[7];
     }
 
     //TODO: extend the map when the mower goes over the boundary
-    //set square = 601 x 601, for maximum size of 200 x 200, no need to expand
+    //set square = 60 x 40, for maximum size of 200 x 200, no need to expand
     private void expand(){
 
     }
