c958c0c05745f6d76db7d15ba3783131ffe78b0a
jiangjun0728 <44887877+jiangjun0728@users.noreply.github.com>
2019-04-07 14:38:25 -0700

    code change

--- Code/src/main/java/Viewer/Action.java
+++ Code/src/main/java/Viewer/Action.java
@@ -17,4 +17,17 @@ public class Action {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getStepSize() {
+        return stepSize;
+    }
+
+    public void setStepSize(int stepSize){ this.stepSize = stepSize;}
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {this.direction = direction;}
+
 }
