ea0ea171a80a00c042613d924f9abbe1f67cf9dd
sylvie910 <sylvie910@gmail.com>
2019-04-09 14:18:40 -0700

    intial update of GUI
    Only static GUI window.

---
+++ Team67-master/Code/src/main/java/Model/RealLawn.java
@@ -0,0 +1,70 @@
+package Model;
+
+import Viewer.Location;
+import Viewer.SquareState;
+
+public class RealLawn extends Lawn {
+    private int width;
+    private int length;
+
+    public void setSquare(Location loc, String status){
+        int x = loc.getX();
+        int y = loc.getY();
+        this.squares[x][y] = SquareState.valueOf(status);
+    }
+
+    public void cutSquare(Location loc){
+        int x = loc.getX();
+        int y = loc.getY();
+        this.squares[x][y] = SquareState.valueOf("empty");
+        this.updateGrassNum();
+    }
+
+    public String getSquareState(Location loc){
+        int x = loc.getX();
+        int y = loc.getY();
+        return squares[x][y].toString();
+    }
+
+    public void initSquares(int[][] pos, String[] state){
+        try {
+            int l;
+            for (int i = 0; i < this.width; i++){
+                for (int j = 0; j < this.length; j++){
+                    Location loc = new Location();
+                    loc.setX(i);
+                    loc.setY(j);
+                    setSquare(loc, "grass");
+                }
+            }
+
+            if ((l = pos.length) == state.length) {
+                for (int i = 0; i < l; i++){
+                    Location loc = new Location();
+                    loc.setX(pos[i][0]);
+                    loc.setY(pos[i][1]);
+                    setSquare(loc, state[i]);
+                }
+            }
+
+        } catch (Exception e){
+            System.out.println("[ERROR]: Invalid initialization of real lawn!");
+        }
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
---
+++ Team67-master/Code/src/main/java/Viewer/SquareState.java
@@ -0,0 +1,13 @@
+package Viewer;
+
+public enum SquareState {
+    grass,
+    empty,
+    crater,
+    fence,
+    puppy_mower,
+    puppy_empty,
+    puppy_grass,
+    mower,
+    unknown
+}
