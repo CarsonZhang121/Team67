5c7fd22b16b990fdd3236e7bb67a046ce012db99
jiangjun0728 <44887877+jiangjun0728@users.noreply.github.com>
2019-04-10 14:07:32 -0700

    code change

---
+++ Code/Code/src/main/java/Model/MowerMap.java
@@ -0,0 +1,77 @@
+package Model;
+
+import Viewer.Direction;
+import Viewer.Location;
+import Viewer.SquareState;
+
+public class MowerMap extends Lawn{
+    private int width = 60;
+    private int length = 40;
+    SquareState[][] map;
+    int remainGrassNumber = 0;
+
+
+    public void initialMap(int width, int length){
+
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < length; j++){
+                map[i][j] = SquareState.unknown;
+            }
+        }
+    }
+
+    //TODO: set square state for the one in mowerMap
+    public void setSquare(Location loc, SquareState sqState, Direction d){
+
+        map[loc.getX()][loc.getY()] = SquareState.empty;
+    }
+
+    //TODO: determine if all the grasses has been cut
+    // What is the logic here???
+    public int remainGrassNumber(){
+        for(SquareState[] a : map){
+            for(SquareState b: a) {
+                if (b == SquareState.grass) {
+                    remainGrassNumber++;
+                }
+            }
+        }
+        return remainGrassNumber;
+    }
+
+    public Boolean isCompleted(MowerMap map){
+        int grassremain = map.remainGrassNumber();
+        Boolean isCompleted = false;
+        if(grassremain == 0){
+            isCompleted = true;
+        }
+        return isCompleted;
+    }
+
+    //TODO: get the square state at a specific location
+    public SquareState getSquare(Location loc){
+
+        SquareState localstate;
+        localstate = map[loc.getX()][loc.getY()];
+        return localstate;
+    }
+
+    //TODO: update the map for each scan
+    public void updateMapFromScan(Location loc, SquareState[] neighbors){
+
+        map[loc.getX()][loc.getY() + 1] = neighbors[0];
+        map[loc.getX() + 1][loc.getY() + 1] = neighbors[1];
+        map[loc.getX() + 1][loc.getY()] = neighbors[2];
+        map[loc.getX() + 1][loc.getY() - 1] = neighbors[3];
+        map[loc.getX()][loc.getY() - 1] = neighbors[4];
+        map[loc.getX() - 1][loc.getY() - 1] = neighbors[5];
+        map[loc.getX() - 1][loc.getY()] = neighbors[6];
+        map[loc.getX() - 1][loc.getY() + 1] = neighbors[7];
+    }
+
+    //TODO: extend the map when the mower goes over the boundary
+    //set square = 60 x 40, for maximum size of 200 x 200, no need to expand
+    private void expand(){
+
+    }
+}
---
+++ Code/Code/src/main/java/Model/Puppy.java
@@ -0,0 +1,81 @@
+package Model;
+
+import Viewer.Location;
+
+import java.util.*;
+import java.io.*;
+
+public class Puppy {
+    //private Location puppyLocations;
+
+    // List the puppyStatus here
+   // private int puppyStatus;
+    private String puppyStatus; // "stay" or "move"
+    private Location puppyLocations;
+
+    double percentToStay;// range from 0 to 1; 1 means always stay; 0 means always move
+    Random randGenForAction;
+    Random randGenForNextMove;
+
+    // Constructor
+    public Puppy(double percentToStay){
+        this.percentToStay = percentToStay;
+        randGenForAction = new Random();
+        randGenForNextMove = new Random();
+    }
+
+ 
+    public String nextAction(String[] neightSquareStates){
+        String actionStr;
+        double randDub = randGenForAction.nextDouble();
+        if (randDub < percentToStay){
+            //stay
+            actionStr = this.puppyStatus = "stay";
+        }else {
+            //move
+            this.puppyStatus = "move";
+            int locIndex = decideNextMoveLocation(neightSquareStates);
+            actionStr = "move" + "," + String.valueOf(locIndex); //e.g.: "move,0"
+
+        }
+        return actionStr;
+    }
+
+    private int decideNextMoveLocation(String[] neightSquareStates){
+        List<Integer> indexes = new ArrayList<>();
+        int randNum;
+        int locIndex;
+        for(int i=0; i < neightSquareStates.length; i++){
+            if(neightSquareStates[i].equals("grass")||
+                    (neightSquareStates[i].equals("empty"))||
+                    (neightSquareStates[i].equals("mower"))){
+                indexes.add(i);
+            }
+        }
+
+        int CeilingNum = indexes.size(); // the number of qualified candidates
+        randNum = randGenForNextMove.nextInt(CeilingNum);
+        locIndex = indexes.get(randNum);
+       // System.out.printf("the num of candidates is %d; the random number is %d;the chosen Locindex is %d\n", CeilingNum,randNum,locIndex);
+        return locIndex;
+    }
+
+
+    public Location getPuppyLocations() {
+        return puppyLocations;
+    }
+
+    public void setPuppyLocations(Location puppyLocations) {
+        this.puppyLocations = puppyLocations;
+    }
+
+    public String getPuppyStatus() {
+        return this.puppyStatus;
+    }
+
+    public void setPuppyStatus(String puppyStatus) {
+        this.puppyStatus = puppyStatus;
+    }
+
+
+}
